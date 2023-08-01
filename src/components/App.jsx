2ab98bbd6d9a6adb4c737a203dbd5eefacdc5b11
MaksymBora <maxboraod@gmail.com>
2023-08-01 20:54:11 +0200

    3rd hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import Profile from './profile'
 import profileData from '../user.json'
 import data from '../data.json'
 import Statistic from './statistic'
+import friendsData from '../friends.json'
+import FriendList from './friendList'
 
 export const App = () => {
   return (
@@ -15,6 +17,8 @@ export const App = () => {
       />
     
       <Statistic title="Upload stats" stats={data} />
+
+      <FriendList props={friendsData} />
     </>
   );
 };
