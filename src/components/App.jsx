312a585722f204d6e7e17acb7bc17931d021eaa6
MaksymBora <maxboraod@gmail.com>
2023-08-05 14:54:09 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import data from '../data.json'
 import Statistic from './statistic/statistic'
 import friendsData from '../friends.json'
 import FriendList from './friendList/friendList'
-// import transactions from '../transactions.json'
+import transactions from '../transactions.json'
 // import TransactionHistory from './transactionHistory/transactionHistory'
 import { GlobalStyle } from './GlobalStyle'
 
