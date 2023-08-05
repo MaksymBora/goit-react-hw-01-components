b28adc1c834b33c495c501e5183ed6d1396d0c9f
MaksymBora <maxboraod@gmail.com>
2023-08-05 14:51:45 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export const App = () => {
 
       <FriendList props={friendsData} />
 
-      <TransactionHistory transactionsItems={transactions} />
+      {/* <TransactionHistory transactionsItems={transactions} /> */}
 
       
     </>
